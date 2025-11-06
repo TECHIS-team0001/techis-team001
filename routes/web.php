60c73edd0405806f75d86cf25176b470c8f4dc3b
non-murakami <dobechan7966@gmail.com>
2025-11-06 12:04:15 +0900

    resolve conflict and set route: top -> menus

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\OrderController;
 
-//トップページ
+// --- トップページ表示 ---
 Route::get('/', function () {
-    return view('top');
-});
+    return view('top'); // resources/views/top.blade.php を表示
+})->name('top');
 
-// メニュー関連
+// --- メニュー関連 ---
 Route::resource('menus', MenuController::class);
 Route::post('/menus/reset', [MenuController::class, 'reset'])->name('menus.reset');
 
-// 注文関連
+// --- 注文関連 ---
 Route::post('/checkout', [OrderController::class, 'checkout'])->name('orders.checkout');
 Route::post('/orders/store', [OrderController::class, 'store'])->name('orders.store');
 Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
+
 
