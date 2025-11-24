303455f9c99626c1ff168628a2ab3f290cd289e6
non-murakami <dobechan7966@gmail.com>
2025-11-24 17:31:42 +0900

    ログイン修正しました

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Staff extends Model
+class Staff extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     protected $table = 'staffs';
 
